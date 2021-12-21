bee69b29dd084989d011bfe7c3ca52906102ec62
JRGCast <joao.ralph1@gmail.com>
2021-12-21 11:54:21 -0300

    no ubuntu apenas o espeak funciona como engine de voz -.-

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ USERNAME = config('THEUSER')
 BOTNAME = config('BOTNAME')
 
 
-engine = pyttsx3.init('sapi5')
+engine = pyttsx3.init('espeak')
 
 # Set Rate
-engine.setProperty('rate', 190)
+engine.setProperty('rate', 180)
 
 # Set Volume
 engine.setProperty('volume', 1.0)
 
 # Set Voice (Female)
+# voices = engine.getProperty('voices')
+# engine.setProperty('voice', voices[1].id)
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+print(len(voices))
+for index, voice in enumerate(voices):
+  print(voice, index)
 
-engine.say('Hello')
+engine.setProperty('voice', voices[53].id)
+engine.say("Meu nome é João, muito prazer")
 engine.runAndWait()
+
+# engine.runAndWait()
 
 print(USERNAME,BOTNAME)
